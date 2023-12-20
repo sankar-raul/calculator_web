349c95d6f5e560c97870b63411182f2d2577dcf6
sankar-coding <raulsankar99@gmail.com>
2023-12-20 17:01:32 +0530

    test

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 app.set("view engine", "ejs");
 app.use(express.static("res"));
 app.get("/",(req,res) => {
