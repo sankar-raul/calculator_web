727d973a65c83f87849fb9f03b10128e4cc68807
sankar-coding <raulsankar99@gmail.com>
2024-03-02 19:32:23 +0530

    😄

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const port = process.env.PORT || 8080;
-app.set("view engine", "ejs");
 app.use(express.static(__dirname));
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname,'404.html'));
+});
 app.get("/",(req,res) => {
    res.sendfile(path.join(__dirname,"index.html"));
 });
