c7baf3b54657edd6393c2f60fdb68c3e8cbf9f45
sankar-coding <raulsankar99@gmail.com>
2024-03-10 17:32:50 +0530

    deply

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const port = process.env.PORT || 8080;
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname,"docs")));
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname,'404.html'));
+  res.status(404).sendFile(path.join(__dirname,"docs",'404.html'));
 });
 app.get("/",(req,res) => {
-   res.sendfile(path.join(__dirname,"index.html"));
+   res.sendfile(path.join(__dirname,"docs","index.html"));
 });
 app.listen(port,() => {
    console.log(`Server is running at port ${port}`);
